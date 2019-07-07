88e0849139b06eb65eb5e48abccb11ab4f85c61f
josephsdavid <josephsd@smu.edu>
2019-07-06 20:28:22 -0500

    did something

---
+++ analysis/xgboostMF.py
@@ -0,0 +1,10 @@
+from xgboost import XGBClassifier
+
+model = XGBClassifier()
+model.fit(X_train, y_train)
+# make predictions for test data
+y_pred = model.predict(X_test)
+predictions = [round(value) for value in y_pred]
+# evaluate predictions
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
